f6f3fd07fe2ee42d660d602bd36389ce74ace305
John-Baird <106564524+John-Baird@users.noreply.github.com>
2022-10-07 10:38:32 -0600

    Working Api Fetching

--- script.js
+++ script.js
@@ -5,51 +5,62 @@ let par = []
 let hcp = []
 let yards = []
 
-    function getAvailableCourses() {
-        fetch('https://golf-courses-api.herokuapp.com/courses/' ,
-        {method: 'GET' ,
-            headers: { 'Content-Type': 'application/json'},
+function getAvailableCourses() {
+    fetch('https://golf-courses-api.herokuapp.com/courses/',
+        {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
         })
-            .then(res => {
+        .then(res => {
             return res.json()
         })
-            .then(data =>  {
-               resolve(data.courses)
-            })
-        }
-        getAvailableCourses();
+        .then(data => {
+            resolve(data.courses)
+        })
+}
+getAvailableCourses();
 
 
 
-function resolve (courses) {
-let courseOptionsHtml = ''
-courses.forEach((course) => {
- courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`
-});
+function resolve(courses) {
+    let courseOptionsHtml = ''
+    courses.forEach((course) => {
+        courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`
+    });
 
-document.getElementById('course-select').innerHTML = courseOptionsHtml
-id = document.getElementById('course-select').value
-console.log(id)
-getGolfCourse()
+    document.getElementById('course-select').innerHTML = courseOptionsHtml
+    id = document.getElementById('course-select').value
+    console.log(id)
+    getGolfCourse()
+
 }
 
 
 
-function getId(){
+function getId() {
     id = document.getElementById('course-select').value
     console.log(id)
 }
 
 const getGolfCourse = () => {
     getId()
-    url = "https://golf-courses-api.herokuapp.com/courses/"+id
-    fetch(url).then((data) => data.json()).then((data) => golfData = data.data.holes)
-    for (let i = 0; i < golfData.length; i++){
+    url = "https://golf-courses-api.herokuapp.com/courses/" + id
+    fetch(url).then((data) => data.json()).then((data) => data.data.holes).then(golfData => {
+    par = []
+    hcp = []
+    yards = []
+    for (let i = 0; i < 18; i++) {
         let a = golfData[i].teeBoxes[1]
-        // teebox[1] is the champion one
+        // teeBoxes[1] is the champion one
         par.push(a.par)
         hcp.push(a.hcp)
         yards.push(a.yards)
     }
+    })
+
 }
 
+
+document.getElementById('course-select').addEventListener('click', function () {
+    getGolfCourse()
+})

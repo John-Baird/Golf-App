3781ed8228d70b1ce10f45e7071b2e209466f157
John-Baird <106564524+John-Baird@users.noreply.github.com>
2022-10-07 09:41:44 -0600

    Working Code - Api Promise Errors

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
 
+let id = ''
+let golfData = []
+let par = []
+let hcp = []
+let yards = []
 
-async function getApi() {
-const data  = await new Promise((resolve, reject) => {
-    fetch('https://golf-courses-api.herokuapp.com/courses/')
-        .then((response) => response.json())
-        .then((data) => {
-            resolve(data)
-        });
-})
+    function getAvailableCourses() {
+        fetch('https://golf-courses-api.herokuapp.com/courses/' ,
+        {method: 'GET' ,
+            headers: { 'Content-Type': 'application/json'},
+        })
+            .then(res => {
+            return res.json()
+        })
+            .then(data =>  {
+               resolve(data.courses)
+            })
+        }
+        getAvailableCourses();
 
-const array = data
 
-return array
+
+function resolve (courses) {
+let courseOptionsHtml = ''
+courses.forEach((course) => {
+ courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`
+});
+
+document.getElementById('course-select').innerHTML = courseOptionsHtml
+id = document.getElementById('course-select').value
+console.log(id)
+getGolfCourse()
 }
 
 
-async function runOnLoad() {
-    bruh = await getApi();
-    counter = 1
-    
-          
+
+function getId(){
+    id = document.getElementById('course-select').value
+    console.log(id)
 }
-counter = 0
-bruh = ''
-window.onload = runOnLoad();
+
+const getGolfCourse = () => {
+    getId()
+    url = "https://golf-courses-api.herokuapp.com/courses/"+id
+    fetch(url).then((data) => data.json()).then((data) => golfData = data.data.holes)
+    for (let i = 0; i < golfData.length; i++){
+        let a = golfData[i].teeBoxes[1]
+        // teebox[1] is the champion one
+        par.push(a.par)
+        hcp.push(a.hcp)
+        yards.push(a.yards)
+    }
+}
+
